7fbcdfc35c50d7fa0923c39002fb773b17a7727b
guido marchini <guido.marchini@gmail.com>
2020-06-26 19:04:33 -0300

    added some objects on startup

--- src/main/kotlin/unq/edu/remotetrainer/RemoteTrainerConfiguration.kt
+++ src/main/kotlin/unq/edu/remotetrainer/RemoteTrainerConfiguration.kt
@@ -1,10 +1,16 @@
 package unq.edu.remotetrainer
 
+import org.joda.time.LocalDate
 import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import unq.edu.remotetrainer.application.sevice.ExerciseBlockService
 import unq.edu.remotetrainer.application.sevice.ExerciseService
+import unq.edu.remotetrainer.application.sevice.RoutineService
 import unq.edu.remotetrainer.model.Exercise
+import unq.edu.remotetrainer.model.ExerciseBlock
+import unq.edu.remotetrainer.model.ExerciseRepetition
+import unq.edu.remotetrainer.model.Routine
 
 /**
  * Application configuration
@@ -14,17 +20,52 @@ class RemoteTrainerConfiguration {
 
     @Bean
     fun dataInitializer(
-        exerciseService: ExerciseService
+        exerciseService: ExerciseService,
+        exerciseBlockService: ExerciseBlockService,
+        routineService: RoutineService
     ) = ApplicationRunner {
-//        exerciseService.createExercise(Exercise(
-//            name = "pullups",
-//            description = "they're hard!"
-//        ))
-//
-//        exerciseService.createExercise(
-//            Exercise(
-//            name = "pushups",
-//            description = "you can start with these ones!"
-//        ))
+        /* Create exercises */
+        val pullups: Exercise =
+            exerciseService.create(Exercise(
+                name = "Dominadas",
+                description = "Son dificiles!"
+            ))
+
+        val pushups =
+            exerciseService.create(Exercise(
+                name = "Flexiones de brazo",
+                description = "Podes empezar con estas. Trabaja ese pectoral"
+            ))
+
+        /* Create exercise blocks */
+        val exerciseBlock =
+            exerciseBlockService.create(ExerciseBlock(
+                name = "Bloque de ejercicios ejemplo",
+                notes = "Hacer los ejercicios por minuto, durante 10 minutos",
+                exercises = mutableListOf(
+                    ExerciseRepetition(
+                        exercise = pushups,
+                        quantity = 10
+                    ),
+                    ExerciseRepetition(
+                        exercise = pullups,
+                        quantity = 5
+                    )
+                )
+            ))
+
+        val routine: Routine =
+            routineService.create(Routine(
+                date = LocalDate.now(),
+                shortDescription = "Principiantes",
+                notes = "Cinco minutos de pausa entre cada serie",
+                exerciseBlocks = mutableListOf(ExerciseBlock(
+                    notes = "Hacer durante un minuto, por 5 minutos",
+                    exercises = mutableListOf(ExerciseRepetition(
+                        exercise = pushups,
+                        quantity = 10
+                    ))
+                ))
+            ))
     }
 }
